9e8e358d92e07272f2e29697ed0af7431d2a1ff1
guglielmobartelloni <bartelloni.guglielmo@gmail.com>
2022-10-27 23:24:34 +0200

    Imrpoved script formatting

--- erome-downloader.sh
+++ erome-downloader.sh
@@ -1,3 +1,5 @@
-#/bin/sh
+#!/bin/sh
 
-yt-dlp --get-url $1 | uniq | aria2c --min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16 --header="Referer: https://www.erome.com/" -i -
+yt-dlp --get-url "$1" | 
+uniq | 
+aria2c --min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16 --header="Referer: https://www.erome.com/" -i -
