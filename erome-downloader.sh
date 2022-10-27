2f58aa09628f909dcb0accad1609c9e788321c1e
guglielmobartelloni <bartelloni.guglielmo@gmail.com>
2022-10-27 23:19:20 +0200

    Improved script now it's all piped

--- erome-downloader.sh
+++ erome-downloader.sh
@@ -1,4 +1,3 @@
 #/bin/sh
 
-yt-dlp --get-url $1 | uniq > videos.txt 
-aria2c --min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16 --header="Referer: https://www.erome.com/" -i videos.txt && rm videos.txt
+yt-dlp --get-url $1 | uniq | aria2c --min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16 --header="Referer: https://www.erome.com/" -i -
